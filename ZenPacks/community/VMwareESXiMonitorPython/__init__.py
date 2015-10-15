b078e56ef8810602f0f13cb9ffa83c883fea88ba
Mattikin <Mattikin@users.noreply.github.com>
2015-10-15 11:46:08 -0400

    The required pyvmomi python libraries are now delivered as part of the ZenPack

--- ZenPacks/community/VMwareESXiMonitorPython/__init__.py
+++ ZenPacks/community/VMwareESXiMonitorPython/__init__.py
@@ -1,4 +1,13 @@
+import os
+import sys
 from . import zenpacklib
+
+libDir = os.path.join(os.path.dirname(__file__), 'lib')
+if os.path.isdir(libDir):
+    sys.path.append(libDir)
+    for file in os.listdir(libDir):
+        if file.endswith('.egg'):
+            sys.path.append(os.path.join(libDir, file))
 
 zenpacklib.load_yaml()
 
